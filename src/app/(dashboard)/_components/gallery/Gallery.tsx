a2200cbd0e47763fbc2b8ee42207c38c7413788d
emeka <emekaowo@yahoo.com>
2024-09-28 18:38:41 +0100

    pix fix

--- src/app/(dashboard)/_components/gallery/Gallery.tsx
+++ src/app/(dashboard)/_components/gallery/Gallery.tsx
@@ -2,21 +2,22 @@ import "./gallery.css"
 
 const GalleryPage = () => {
     return ( 
-    <div className="gallery-cover"> 
+      <div className="py-8">
+         <div className="gallery-cover h-[50vh] p-2"> 
       <div className="levels">
-        <h2>MEETING YOU AT ALL LEVELS OF <span>LEARNING</span></h2>
-        <h4>Nursery | Primary | College | Extra-Mural Classes | The Bethel University</h4>
+        <h2 className=" text-[16px]">DANGLOBAL PETROLEUM LTD <span className="font-bold">DELIVERS</span></h2>
+        <h4 className=" text-[16px]">Products | Importation | Leasing | Exploration | Labor Providers</h4>
       </div>
-      <div className="one">
-        <div className="two unit"><img src="https://images.pexels.com/photos/1181516/pexels-photo-1181516.jpeg? 
-            auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+      <div className="one p-0">
+        <div className="two unit"><img src="https://media.istockphoto.com/id/155356841/photo/tanker-truck-on-a-highway.jpg?b=1&s=612x612&w=0&k=20&c=efmyl1gHQGFd38WPYxbDmkpxUCjAkcHpQQSysD47ZT4=" alt=""/>
         </div>
-        <div className="three unit"><img src="https://cdn.ahzassociates.co.uk/wp-content/uploads/2023/08/02180701/nigerian-student.webp" alt=""/></div>
-        <div className="four unit"><img src="https://solacebase.com/wp-content/uploads/2020/02/JAMB-Exam-1.jpg" alt=""/></div>
-        <div className="five unit"><img src="https://ryculture.org/wp-content/uploads/2021/10/Image-YVN.jpg" alt="somethin"/></div>
-        <div className="seven unit"><img src="https://edusko-dev-bucket.s3.eu-north-1.amazonaws.com/1635113410523IvJAQJ3LyGO3p8y0R-YPhuOYF8fEM1HE.png" alt=""/></div>
+        <div className="three unit"><img src="https://images.pexels.com/photos/6005876/pexels-photo-6005876.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/></div>
+        <div className="four unit"><img src="https://media.istockphoto.com/id/1199749863/photo/retail-gasoline-station-and-convenience-store-reworked.jpg?b=1&s=612x612&w=0&k=20&c=IjBfbwGYFQDwB_CyruOQy4NJ-MjVq96YWnnu8b_jEGk=" alt=""/></div>
+        <div className="five unit"><img src="https://images.pexels.com/photos/327041/pexels-photo-327041.jpeg?auto=compress&cs=tinysrgb&w=600" alt="somethin"/></div>
+        <div className="seven unit"><img src="https://images.pexels.com/photos/7932921/pexels-photo-7932921.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/></div>
       </div>
-    </div>
+         </div>
+      </div>
 );
 }
  
